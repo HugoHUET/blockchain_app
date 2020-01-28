a6dd32390291c78a0a63b4f578ad38689532e000
yavuz41 <51058615+yavuz41@users.noreply.github.com>
2020-01-28 22:49:31 +0100

    Add files via upload

--- pdf_to_txt_script.py
+++ pdf_to_txt_script.py
@@ -1,15 +1,27 @@
+import requests
 import PyPDF2 as o
 openPDF = open("Bel_Ami.pdf","rb")
 pdfRead = o.PdfFileReader(openPDF)
 print(pdfRead.getPage(0).extractText())
 print(pdfRead.getNumPages())
-print(pdfRead.isEncrypted)
-print(pdfRead.getDocumentInfo())
+
 
 i=0
+
+pages_arr = []
+pages = {}
 
 while i < pdfRead.getNumPages():
     page = pdfRead.getPage(i)
     text = page.extractText()
     print(text)
     i = i+1
+    pages = {
+        'numero_page': page,
+        'texte': text,
+    }
+    pages_arr.append(pages)
+    if len(pages_arr)==4:
+        print(pages_arr)
+        requests.post("http://127.0.0.1:5000/create-transaction", json = pages_arr)
+        pages_arr= []
